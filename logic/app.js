d4163bff7d565fbd0b2853ed8af15399ead23fa2
lukas simianer <lsimianer@gmail.com>
2019-10-12 14:06:52 -0500

    db responding with users on signup rpute, hallllo

--- logic/app.js
+++ logic/app.js
@@ -1,42 +1,46 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const app = express();
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const expressValidator = require('express-validator')
 require('dotenv').config()
-
 // import routes
 const userRoutes = require('../routes/user.js');
-const port = process.env.PORT || 8000
+//database connection
+    mongoose.connect(process.env.DATABASE, {
+        useUnifiedTopology: true,
+        useNewUrlParser: true,
+        useCreateIndex: true
+    }).then(() => console.log('DB Connected'))
 
-//db 
-mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useCreateIndex: true
-}).then(() => console.log('DB Connected'))
+//app 
+const app = express();
+//middlwares
+app.use(morgan("dev"));
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(expressValidator());
+
 
 // routes middleware
 app.use("/api", userRoutes)
+//db 
 
-// get('/' , (req, res) =>{
-//     res.send('hello from node');
+
+// app.post("/signup", function (req, res) {
+//     console.log(req.body);
+//     res.send({
+//         status: 'SUCCESS'
 //     });
-    
-
-//  import mongooseconst mongoose = require('mongoose');
-//   load env variablesconst dotenv = require('dotenv');dotenv.config() 
-//   db connectionmongoose.connect(  process.env.MONGO_URI,  {useNewUrlParser: true}).then(() => console.log('DB Connected'))
-//    mongoose.connection.on('error', err => {  console.log(`DB connection error: ${err.message}`)});
+// });
 
 
+
+
+
+const port = process.env.PORT || 8000
 
 app.listen(port, () => {
     console.log(`server is running on port ${port}`);
 })
-
-// const MongoClient = require('mongodb').MongoClient;
-// const uri = "mongodb+srv://lsimianer:<password>@cluster0-7z1it.mongodb.net/admin?retryWrites=true&w=majority";
-// const client = new MongoClient(uri, { useNewUrlParser: true });
-// client.connect(err => {
-//   const collection = client.db("test").collection("devices");
-//   // perform actions on the collection object
-//   client.close();
-// });
