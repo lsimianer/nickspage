c44243e1796e6dc86a5f8707cda45565879569da
lukas simianer <lsimianer@gmail.com>
2019-10-19 13:09:03 -0500

    nav and links added to each bage 60% ish of wireframe

---
+++ client/src/pages/pay/Pay.js
@@ -0,0 +1,60 @@
+import React, {Component} from "react";
+import { Link } from "react-router-dom";
+import "./pay.css";
+import brtext from'../images/brtext.jpg';
+// import API from "../../utils/API.js";
+
+class Pay extends Component {
+
+//     state = {
+//         developers : {}
+//     }
+
+//     componentDidMount() {
+//         API.getDeveloperMe()
+//           .then(res => {
+//               console.log("FINDME");
+//               console.log(res.data)
+//               this.setState({ developers: res.data } )
+//         })
+//           .catch(err => console.log(err));
+//       }
+
+    
+
+    render() {
+    return (
+        <div>
+            <nav class="navbar navbar-expand-lg navbar-custom">
+                <Link to={"/"} id="navImg" class="navbar-left">
+            <img id="navImg" src={brtext}/></Link>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item active">
+                    <Link class="nav-link" to={"/Home"}>Home</Link>
+                </li>
+                <li class="nav-item">
+                    <Link class="nav-link" to={"/Calendar"}>Book Training</Link>
+                </li>
+                <li class="nav-item">
+                    <Link class="nav-link" to={"/Store"}>Buy Swag</Link>
+                </li>
+                <li class="nav-item">
+                    <Link class="nav-link" to={"/results"}>Client Success</Link>
+                </li>
+                <li class="nav-item">
+                    <Link class="nav-link" to={"Education"}>Education</Link>
+                </li>
+            </ul>
+        </div>
+        </nav>
+        </div>
+    );
+    }
+}
+
+export default Pay;
