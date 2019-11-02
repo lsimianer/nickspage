2b7cc931891c8743d343bcb66105e75102fae717
lukas simianer <lsimianer@gmail.com>
2019-11-02 17:05:15 -0500

    pics in, adding to carousel

--- src/pages/results/carousel copy.js
+++ src/pages/results/carousel copy.js
@@ -2,16 +2,16 @@ import React, {Component} from 'react';
 import "./results.css";
 import Coverflow from 'react-coverflow';
 import p1 from '../images/transformation1';
-// import p2 from '../images/transformation2';
-// import p3 from '../images/transformation3';
-// import p4 from '../images/transformation4';
-// import p5 from '../images/transformation5';
-// import p6 from '../images/transformation6';
-// import p7 from '../images/transformation7';
-// import p8 from '../images/transformation8';
-// import p9 from '../images/transformation9';
-// import p10 from '../images/transformation10';
-// import p11 from '../images/transformation11';
+import p2 from '../images/transformation2';
+import p3 from '../images/transformation3';
+import p4 from '../images/transformation4';
+import p5 from '../images/transformation5';
+import p6 from '../images/transformation6';
+import p7 from '../images/transformation7';
+import p8 from '../images/transformation8';
+import p9 from '../images/transformation9';
+import p10 from '../images/transformation10';
+import p11 from '../images/transformation11';
 
 
 const fn = function () {
@@ -56,11 +56,11 @@ class HomeCarousel extends Component {
             role="menuitem"
             tabIndex="0"
           >
-            {/* <img id="carouselPic" src={p6} alt='transformation pic'/> */}
+            <img id="carouselPic" src={p6} alt='transformation pic'/>
 
           </div>
           <img id="carouselPic" src={p1} alt='transformation pic'/>
-          {/* <img id="carouselPic" src={p2} alt='transformation pic'/>
+          <img id="carouselPic" src={p2} alt='transformation pic'/>
           <img id="carouselPic" src={p3} alt='transformation pic'/>
           <img id="carouselPic" src={p4} alt='transformation pic'/>
           <img id="carouselPic" src={p5} alt='transformation pic'/>
@@ -69,7 +69,7 @@ class HomeCarousel extends Component {
           <img id="carouselPic" src={p8} alt='transformation pic'/>
           <img id="carouselPic" src={p9} alt='transformation pic'/>
           <img id="carouselPic" src={p10} alt='transformation pic'/>
-          <img id="carouselPic" src={p11} alt='transformation pic'/> */}
+          <img id="carouselPic" src={p11} alt='transformation pic'/>
          
         </Coverflow>
       </div>
